3d867c34d9db507223c5cd7f5f97aa1976176627
joaorato <jpedrorato@tecnico.ulisboa.pt>
2019-11-16 21:26:36 +0000

    late changes

--- Lab3/MLP.py
+++ Lab3/MLP.py
@@ -15,6 +15,10 @@ y_train = np.load('mnist_train_labels.npy')
 x_test = np.load('mnist_test_data.npy')
 y_test = np.load('mnist_test_labels.npy')
 
+# plt.imshow(x_train[1,:,:,0])
+# plt.show()
+# print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
 #from greyscale (0-255) to (0-1)
 x_train = x_train/255
 x_test = x_test/255
@@ -30,8 +34,8 @@ y_test_matrix = keras.utils.to_categorical(y_test, num_classes = 10, dtype = 'in
 
 X_train, X_val, Y_train, Y_val = sklearn.model_selection.train_test_split(x_train, y_train_matrix, test_size=0.30, random_state=42)
 
-#plt.imshow(x_train[1,:,:,0])
-#plt.show()
+# plt.imshow(x_train[1,:,:,0])
+# plt.show()
 
 print(X_train.shape)
 
@@ -45,8 +49,8 @@ print(model.summary())
 
 earlyStopping = [EarlyStopping(monitor='val_loss', patience=15, restore_best_weights=True)]
 model.compile(loss='categorical_crossentropy', optimizer=optimizer.Adam(lr = 0.01, clipnorm = 1))
-history = model.fit(X_train, Y_train, batch_size=300, epochs=400, callbacks=earlyStopping, validation_data=(X_val, Y_val))
-#history = model.fit(X_train, Y_train, batch_size=300, epochs=400, validation_data=(X_val, Y_val)) #without early stopping
+#history = model.fit(X_train, Y_train, batch_size=300, epochs=400, callbacks=earlyStopping, validation_data=(X_val, Y_val))
+history = model.fit(X_train, Y_train, batch_size=300, epochs=400, validation_data=(X_val, Y_val)) #without early stopping
 
 #print(history.history.keys())
 plt.plot(history.history['loss'], label="Training Loss")
@@ -58,7 +62,12 @@ plt.show()
 
 y_predicted = model.predict(x_test)
 
-accuracy = sklearn.metrics.accuracy_score(y_test_matrix, y_predicted.round())
+for i in range(500):
+    index = y_predicted[i].argmax()
+    y_predicted[i] = [0,0,0,0,0,0,0,0,0,0]
+    y_predicted[i,index] = 1
+
+accuracy = sklearn.metrics.accuracy_score(y_test_matrix, y_predicted)
 print("accuracy = ", accuracy)
 conf_matrix = sklearn.metrics.confusion_matrix(y_test_matrix.argmax(axis=1), y_predicted.argmax(axis=1))
 print("Confusion matrix : \n", conf_matrix)
